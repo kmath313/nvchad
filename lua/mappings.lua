7caf9a82779378803b73bf2102cede6efe07ffe5
kmath313 <91571787+kmath313@users.noreply.github.com>
2024-10-03 16:02:04 +1000

    Update mappings.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,5 +7,11 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "gg", ":Neogit<CR>", { desc = "Neogit" })
+map("n", "<RightMouse>", function()
+  vim.cmd.exec '"normal! \\<RightMouse>"'
+
+  local options = vim.bo.ft == "NvimTree" and "nvimtree" or "default"
+  require("menu").open(options, { mouse = true })
+end, {})
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
