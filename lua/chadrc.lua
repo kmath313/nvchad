2c1f767ce7438ce46913a126d2e42d984d2a4d62
kmath313 <91571787+kmath313@users.noreply.github.com>
2024-10-03 13:28:13 +1000

    Update chadrc.lua

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,20 +5,21 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.base46 = {
-	theme = "catppuccin",
-	transparency = "true",
-	integrations = { "neogit" },
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
-}
-
-M.ui = {
-	nvdash = {
-		load_on_startup = true,
+M = {
+	base46 = {
+		theme = "catppuccin",
+		transparency = "true",
+		integrations = { "neogit" },
+	
+		-- hl_override = {
+		-- 	Comment = { italic = true },
+		-- 	["@comment"] = { italic = true },
+		-- },
+	},
+	ui = {
+		nvdash = {
+			load_on_startup = true,
+		},
 	},
 }
 
