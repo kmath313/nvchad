658b254c0058126d855fabe62f22519a8b8369be
kmath313 <91571787+kmath313@users.noreply.github.com>
2024-10-07 17:46:00 +1100

    Update lspconfig.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "gopls", "nil_ls", "yamlls", "hyprls" "jsonls" }
+local servers = { "html", "cssls", "gopls", "nil_ls", "yamlls", "hyprls", "jsonls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
